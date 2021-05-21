a8121dbb413a0f0a7fd0834b54c51a85a574bfe2
yangjie1071 <yangjie1071@163.com>
2021-05-21 22:04:25 +0800

    Merge branch 'master' of github.com:mumu1071/Node-Project
    # Conflicts:
    #	controller/map-controller.js
    #	routes/map.js

--- core/cache.js
+++ core/cache.js
@@ -3,9 +3,11 @@
 const LRU = require('lru-cache')
 
 const mcache = new LRU({
-    max: 100 * 1024 * 1024,
-    maxAge: 1000 * 60 * 5,
-    length: function(n, key) { return n.length + key.length }
+    max: 100 * 1024 * 1024 * 2,
+    maxAge: 1000 * 60 * 60 * 24 * 10,
+    length: function (n, key) {
+        return n.length + key.length
+    }
 })
 
 module.exports = mcache
